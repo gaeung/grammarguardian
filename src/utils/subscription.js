cb7f128fda0b86912ccf7d329d5d35c30425d4e7
Gaeun <gagaeun@gmail.com>
2023-09-13 19:12:08 +0900

    [HOTFIX] 정기 구독 갱신을 찾는 쿼리 수정 / subscription.js에 누락된 함수 추가 / 에러 핸들링을 위한 try-catch 추가

--- src/utils/subscription.js
+++ src/utils/subscription.js
@@ -1,6 +1,8 @@
 const cron = require("node-cron");
 const axios = require("axios");
+const moment = require("moment-timezone");
 
+const paymentDao = require("../models/paymentDao");
 const subscriptionDao = require("../models/subscriptionDao");
 const logger = require("./logger");
 const { throwCustomError } = require("../utils/error");
@@ -15,18 +17,30 @@ const SubscriptionPrice = Object.freeze({
   3: 50000,
 });
 
+function getEndDate(date) {
+  const utcTime = moment.tz(date, "UTC");
+  const kstTime = utcTime.tz("Asia/Seoul");
+  const endDate = kstTime.add(30, "days").format("YYYY-MM-DD HH:mm:ss");
+
+  return endDate;
+}
+
 const retryOnError = async (func, retries = 3) => {
   let attempts = 0;
+  let lastError;
 
   while (attempts < retries) {
     try {
       return await func();
     } catch (err) {
+      lastError = err;
       attempts++;
       logger.warn(`Retry ${attempts} failed, retrying...`);
     }
   }
-  throw new Error("Max retries reached");
+
+  logger.error(`Max retries reached: ${lastError.message}`);
+  throwCustomError("Max retries reached", 400);
 };
 
 const subscription = () => {
@@ -44,60 +58,66 @@ const subscription = () => {
         const planId = obj.planId;
 
         const processPayment = async () => {
-          const kakaoPayProcess = await axios.post(
-            "https://kapi.kakao.com/v1/payment/subscription",
-            {},
-            {
-              headers: {
-                Authorization: `KakaoAK ${process.env.KAKAO_ADMIN_KEY}`,
-                "Content-type":
-                  "application/x-www-form-urlencoded;charset=utf-8",
-              },
-              params: {
-                cid: process.env.KAKAO_CID,
-                sid: sid,
-                partner_order_id: process.env.KAKAO_PARTNER_ORDER_ID,
-                partner_user_id: process.env.KAKAO_PARTNER_USER_ID,
-                item_name: PlanName[planId],
-                quantity: 1,
-                total_amount: SubscriptionPrice[planId],
-                vat_amount: Math.round((SubscriptionPrice[planId] * 10) / 110),
-                tax_free_amount: 0,
-              },
+          try {
+            const kakaoPayProcess = await axios.post(
+              "https://kapi.kakao.com/v1/payment/subscription",
+              {},
+              {
+                headers: {
+                  Authorization: `KakaoAK ${process.env.KAKAO_ADMIN_KEY}`,
+                  "Content-type":
+                    "application/x-www-form-urlencoded;charset=utf-8",
+                },
+                params: {
+                  cid: process.env.KAKAO_CID,
+                  sid: sid,
+                  partner_order_id: process.env.KAKAO_PARTNER_ORDER_ID,
+                  partner_user_id: process.env.KAKAO_PARTNER_USER_ID,
+                  item_name: PlanName[planId],
+                  quantity: 1,
+                  total_amount: SubscriptionPrice[planId],
+                  vat_amount: Math.round(
+                    (SubscriptionPrice[planId] * 10) / 110
+                  ),
+                  tax_free_amount: 0,
+                },
+              }
+            );
+
+            if (kakaoPayProcess.status !== 200) {
+              throwCustomError(`Payment failed for userId: ${userId}`, 400);
             }
-          );
 
-          if (kakaoPayProcess.status === 200) {
-            const paymentMethod = kakaoPayProcess.data.payment_method_type;
-            const amount = kakaoPayProcess.data.amount.total;
+            if (kakaoPayProcess.status === 200) {
+              const paymentMethod = kakaoPayProcess.data.payment_method_type;
+              const amount = kakaoPayProcess.data.amount.total;
 
-            const paymentStatus = "Completed";
-            const { endDate } = getStartAndEndDate(
-              kakaoPayProcess.data.approved_at
-            );
+              const paymentStatus = "Completed";
+              const endDate = getEndDate(kakaoPayProcess.data.approved_at);
 
-            await paymentDao.createPayment(
-              userId,
-              tid,
-              sid,
-              planId,
-              paymentMethod,
-              paymentStatus,
-              amount
-            );
+              await paymentDao.createPayment(
+                userId,
+                tid,
+                sid,
+                planId,
+                paymentMethod,
+                paymentStatus,
+                amount
+              );
 
-            await subscriptionDao.updateSubscription(
-              userId,
-              sid,
-              planId,
-              endDate
-            );
+              await subscriptionDao.updateSubscription(
+                userId,
+                sid,
+                planId,
+                endDate
+              );
 
-            logger.info(
-              `Payment completed for userId: ${userId}, amount: ${amount}`
-            );
-          } else {
-            throwCustomError(`Payment failed for userId: ${userId}`, 400);
+              logger.info(
+                `Payment completed for userId: ${userId}, amount: ${amount}`
+              );
+            }
+          } catch (error) {
+            throw error;
           }
         };
 
